dfeffaf29c6709149da0979927977b5da8b88c78
shagunsharma6677 <shagunsharma6677@gmail.com>
2023-05-31 11:08:44 +0530

    final2

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import img from "../../../public/5494.jpg";
 import Header from "../Header/Header";
 import { Button, Heading } from "@chakra-ui/react";
@@ -18,10 +18,11 @@ const Hero = () => {
               <Button className="btn" colorScheme="red" borderRadius={"3xl"}>
                 Start Your healthy Lifestyle
               </Button>
+
               <Heading fontSize={"5xl"} lineHeight={"52px"} color={"black"}>
                 BEGIN YOUR TRANSFORMATION TODAY
               </Heading>
-              <p class="leading-5  max-w-sm m-4 ml-0 text-base">
+              <p class="leading-5   max-w-sm m-4 ml-0 text-base">
                 Locavore cardigan small batch roof party blue bottle blog
                 meggings sartorial jean shorts kickstarter migas sriracha
                 church-key synth succulents. Actually taiyaki neutra, distillery
